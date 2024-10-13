7fac939b5a0d62c10c54cfcb2a8ed8fb377e4b38
Kinjal Agarwal <kinjal.agarwal@deltatech.gg>
2024-10-13 17:47:12 +0530

    test cases added

--- src/app/string-calculator/string-calculator.component.spec.ts
+++ src/app/string-calculator/string-calculator.component.spec.ts
@@ -11,10 +11,29 @@ describe('StringCalculatorComponent', () => {
 
     const fixture = TestBed.createComponent(StringCalculatorComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges(); 
   });
 
   it('should return 0 for an empty string', () => {
     expect(component.addNumbers("")).toBe(0);
   });
+  it('should return the sum of two numbers separated by a comma', () => {
+    expect(component.addNumbers("1,2")).toBe(3);
+  });
+  it('should handle new lines as valid separators', () => {
+    expect(component.addNumbers("1\n2,3")).toBe(6);
+  });
+  it('should support custom delimiters', () => {
+    expect(component.addNumbers("//;\n1;2")).toBe(3);
+  });
+  it('should throw an exception for negative numbers', () => {
+    expect(() => component.addNumbers("1,-2,3,-4")).toThrow(
+      new Error("Negative numbers not allowed: -2, -4")
+    );
+  });
+  it('should handle multiple numbers', () => {
+    expect(component.addNumbers("1,2,3,4,5")).toBe(15);
+  });
+
   
 });
